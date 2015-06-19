5921047360a498602ac3d6487dcb5e0ffcc2e49f
Michael Weinberg <michaelweinberg@users.noreply.github.com>
2015-06-19 16:19:39 -0700

    Fixed bug in server.js

--- 052615/server.js
+++ 052615/server.js
@@ -24,20 +24,25 @@ server.route({
 	path:"/",
 	handler:function(req,reply){
 		console.log(req.state);
-		if(!req.state.user){
+		if(!req.state.username){
+			console.log("no user");
 			return reply.redirect("/login");
 		}
 		db.get("SELECT * FROM auth WHERE username = $user",{
-			$user:req.state.user
+			$user:req.state.username
 		},function(err,result){
 			console.log(err);
+			console.log(req.state.username);
 			if(!result){
+				console.log("no result");
 				return reply.redirect("/login");
 			}
 			if(result.session != req.state.session){
+				console.log("no session");
 				return reply.redirect("/login");
 			}
-			reply("it's a mystery");
+			reply("it is a mystery");
+			console.log("logged in");
 		});
 	}
 });
@@ -48,7 +53,7 @@ server.route({
 	handler:function(req,reply){
 		reply(
 			"<form method=POST>" +
-			"<input name = user placeholder=user/>" +
+			"<input name =user placeholder=user/>" +
 			"<input name =password placeholder=password/>" +
 			"<input type = submit>"+
 			"</form>"
@@ -62,10 +67,11 @@ server.route({
 	handler:function(req,reply){
 		var expected = users[req.payload.user];
 		if (req.payload.password == expected){
-			var response = reply("worked");
+			var response = reply("hello, " + req.payload.user);
 			var id = Date.now();
 			response.state("username", req.payload.user);
 			response.state("session", id + "");
+			console.log(response.request.state.username);
 			
 			db.run("DELETE FROM auth where USERNAME = $user", {
 				$user:req.payload.user

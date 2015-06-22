d9ff4cd00ca531e20fdaf40080a517592fe736ec
Michael Weinberg <michaelweinberg@users.noreply.github.com>
2015-06-22 14:40:46 -0700

    Added comments to view

--- 051812/calcView.js
+++ 051812/calcView.js
@@ -9,8 +9,13 @@ var CalculatorView = Backbone.View.extend({
 				},
 				template:_.template($("#calc-template").html()),
 				render:function(){
+					//get data from model
 					var model = this.model.toJSON();
+					//put the current data into this template
 					var html = this.template(model);
+					//set the contents of the element to be that html
+					//this.$el is created by jquery from our el: key
+					//set the contents of our div = to the output of the template
 					this.$el.html(html);
 				},
 				updateInput: function(e){
